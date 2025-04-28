0a7dc84a0744328ac1031f0d1bd45e6b2564f625
Augusto Gilioli <augustolg9@gmail.com>
2025-04-28 17:20:39 -0400

    criado categoria

--- VelozRent/settings.py
+++ VelozRent/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'categoria',  # <-- Adicione o nome do seu aplicativo aqui
 ]
 
 MIDDLEWARE = [
