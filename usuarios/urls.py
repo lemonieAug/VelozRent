4be8ce629904e1fbe9408ef6b953cd7f9d77e847
Augusto Gilioli <augustolg9@gmail.com>
2025-04-30 17:20:31 -0400

    .

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     #path('profile/', views.profile, name='profile'),
     #path('update_profile/', views.update_profile, name='update_profile'),
-    ##path('change_password/', auth_views.PasswordChangeView.as_view(template_name='usuarios/change_password.html'), name='change_password'),
+    #path('change_password/', auth_views.PasswordChangeView.as_view(template_name='usuarios/change_password.html'), name='change_password'),
     #path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(template_name='usuarios/password_change_done.html'), name='password_change_done'),
 ]
