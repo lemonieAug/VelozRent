919df285aafc46ec581f850326d0f377d12012db
Augusto Gilioli <augustolg9@gmail.com>
2025-04-30 14:39:07 -0400

    update

--- veiculos/urls.py
+++ veiculos/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('carros/<slug:slug>/', views.carros_por_categoria, name='carros_por_categoria'),
+    #path('<slug:slug>/', views.carros_por_categoria, name='carros_por_categoria'),
     path('', views.carros_disponiveis, name='carros'),  # Página de listagem de carros
     path('<slug:slug>/', views.detalhe_carro, name='detalhe_carro'),  # Página de detalhe do carro
 ]
